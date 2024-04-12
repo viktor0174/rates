b05070541e0832b1b6b18b57b6476e672ec2202b
viktor0174 <viktor.01.74@mail.ru>
2024-04-12 17:10:54 +0500

    work on jquery

--- views/site/rates.php
+++ views/site/rates.php
@@ -1,23 +1,35 @@
-
-<table>
-        <thead>
-        <tr>
-            <th>№</th>
-            <th>Название</th>
-            <th>Описание</th>
-            <th>Скорость</th>
-            <th>Цена</th>
+<style>
+    .ratesList tr td {cursor:default;}
+    .edit {cursor:text !important; }
+</style>
+<table class="ratesList" cellspacing="5" cellpadding="5">
+    <thead>
+    <tr>
+        <th>№</th>
+        <th>Название</th>
+        <th>Описание</th>
+        <th>Скорость </th>
+        <th>Цена</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?foreach($queryRates as $value):?>
+        <tr id="rate<?=$value->id?>">
+            <td><?=$value->id?></td>
+            <td><?=$value->name?></td>
+            <td><?=$value->description?></td>
+            <td id="editSpeed<?=$value->id?>" contentEditable="true" class="edit" title="Отредактируйте скорость как Вам нужно"><?=$value->speed?> Мбит/с</td>
+            <td id="Price<?=$value->id?>"><?=$value->price?> ₽</td>
         </tr>
-        </thead>
-        <tbody>
-            <?foreach($queryRates as $value):?>
-            <tr>
-                <td><?=$value->id?></td>
-                <td><?=$value->name?></td>
-                <td><?=$value->description?></td>
-                <td><?=$value->speed?></td>
-                <td><?=$value->price?></td>
-            </tr>
-            <?endforeach;?>
-        </tbody>
-    </table>
+        <?endforeach;?>
+    </tbody>
+</table>
+<?
+$this->registerJs(
+    "$('.edit').on('focusout',function () {let res=confirm('Сохранить внесенные изменения?'); if(res){
+        let txt = $(this).text();
+        alert(txt+' =>ok');
+    }else{return false;}
+    });"
+);
+?>
