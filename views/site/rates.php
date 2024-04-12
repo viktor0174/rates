48d6308e4454fd72b3720eaf9a7ef192a7697d4c
viktor0174 <viktor.01.74@mail.ru>
2024-04-12 11:48:31 +0500

    create htaccess and add script to it

---
+++ views/site/rates.php
@@ -0,0 +1,20 @@
+
+<table>
+        <thead>
+        <tr>
+            <th>№</th>
+            <th>Название</th>
+            <th>Скорость</th>
+            <th>Цена</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+    </table>
+    <?= __FILE__ ?>
